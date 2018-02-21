c16e20279a24d8069a29e26febd5aab036c674ea
ErnestoPM <ernestopm20@gmail.com>
2018-02-20 21:56:21 -0600

    Adding guidelines, new colors and buttons

--- app/src/main/java/com/example/ernesto/finanzapp/SummaryFragment.java
+++ app/src/main/java/com/example/ernesto/finanzapp/SummaryFragment.java
@@ -9,9 +9,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -43,7 +46,7 @@ public class SummaryFragment extends Fragment {
 
         List<BarEntry> entries = new ArrayList<>();
         entries.add(new BarEntry(0f, 30f));
-        entries.add(new BarEntry(1f, 80f));
+        entries.add(new BarEntry(1f, 100f));
         entries.add(new BarEntry(2f, 60f));
         entries.add(new BarEntry(3f, 50f));
         // gap of 2f
@@ -51,11 +54,31 @@ public class SummaryFragment extends Fragment {
         entries.add(new BarEntry(6f, 60f));
 
         BarDataSet dataSet = new BarDataSet(entries, "Bar Chart example");
+        dataSet.setColors(ColorTemplate.MATERIAL_COLORS);
 
         BarData data = new BarData(dataSet);
         data.setBarWidth(0.9f);
         chart.setData(data);
         chart.setFitBars(true);
+
+        // Styling
+
+        // X axis
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextSize(10f);
+        xAxis.setDrawAxisLine(true);
+        xAxis.setDrawGridLines(false);
+
+
+        // Y axis
+        YAxis left = chart.getAxisLeft();
+        left.setDrawGridLines(false);
+        left.setDrawAxisLine(true);
+        chart.getAxisRight().setEnabled(false);
+
+        chart.animateY(700);
+
         chart.invalidate();
     }
 }
